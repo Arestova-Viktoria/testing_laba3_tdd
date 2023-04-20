463320bfda04a9cb2f97656532820113fc7e4c3e
Viktoria <vika_are@mail.ru>
2023-04-20 20:06:30 +0700

    linear equation test negative

--- tests/test_math_calculator.py
+++ tests/test_math_calculator.py
@@ -14,6 +14,14 @@ class MathCalculatorTest(unittest.TestCase):
         result = -2.5
         self.assertEqual(solution, result)
 
+    def test_linear_equation_a_0(self):
+        with self.assertRaises(ZeroDivisionError) as e:
+            mc = Math_Calculator()
+            a = 0
+            b = 5
+            solution = mc.linear_equation(a, b)
+        self.assertEqual('Уравнение не имеет корней',e.exception.args[0])
+
 
 if __name__ == '__main__':
     unittest.main()
